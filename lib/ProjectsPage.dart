18d394d2586e9c1a2a0747fb6ed43187712f7c39
Ruitenbeek <mvanderuitenbeek@vwinfra.nl>
2019-07-08 21:35:59 +0200

    added live connection

--- lib/ProjectsPage.dart
+++ lib/ProjectsPage.dart
@@ -47,3 +47,86 @@ class DetailedProjectPage extends StatelessWidget {
     );
   }
 }
+
+@override
+Widget build(BuildContext context) {
+  return new Scaffold(
+    body: GoogleMap(
+      mapType: MapType.hybrid,
+      initialCameraPosition: _kGooglePlex,
+      onMapCreated: (GoogleMapController controller) {
+        _controller.complete(controller);
+      },
+    ),
+    floatingActionButton: FloatingActionButton.extended(
+      onPressed: _goToTheLake,
+      label: Text("'Collega's"),
+      icon: Icon(Icons.transfer_within_a_station),
+    ),
+  );
+}
+
+Future<void> _goToTheLake() async {
+  final GoogleMapController controller = await _controller.future;
+  controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+}
+}
+
+Widget _buildBody(BuildContext context) {
+  return StreamBuilder<QuerySnapshot>(
+    stream: Firestore.instance.collection('projects').snapshots(),
+    builder: (context, snapshot) {
+      if (!snapshot.hasData) return LinearProgressIndicator();
+
+      return new Scaffold(
+          body: _buildList(context, snapshot.data.documents)
+      );
+    },
+  );
+}
+
+Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
+  return ListView(
+    padding: const EdgeInsets.only(top: 20.0),
+    children: snapshot.map((data) => _buildListItem(context, data)).toList(),
+  );
+}
+
+Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
+  final record = Record.fromSnapshot(data);
+
+  return Padding(
+    key: ValueKey(record.name),
+    padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+    child: Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(5.0),
+      ),
+      child: ListTile(
+        title: Text(record.name),
+        trailing: Text(record.worksite),
+        onTap: () => print(record),
+      ),
+    ),
+  );
+}
+
+
+class Record {
+  final String name;
+  final String worksite;
+  final DocumentReference reference;
+
+  Record.fromMap(Map<String, dynamic> map, {this.reference})
+      : assert(map['name'] != null),
+        assert(map['worksite'] != null),
+        name = map['name'],
+        worksite = map['worksite'];
+
+  Record.fromSnapshot(DocumentSnapshot snapshot)
+      : this.fromMap(snapshot.data, reference: snapshot.reference);
+
+  @override
+  String toString() => "Record<$name:$worksite>";
+}
